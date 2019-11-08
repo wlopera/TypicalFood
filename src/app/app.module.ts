1cde2e6df10831d1a1e71c71ddbee20a2b71f673
William Lopera <lopera.william@gmail.com>
2019-11-07 22:06:18 -0500

    Ajuste uso de boopstrap-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FoodsComponent } from './foods/foods.component';
 import { FoodDetailsComponent } from './food-details/food-details.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodsComponent,
-    FoodDetailsComponent
+    FoodDetailsComponent,
+    HomeComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
