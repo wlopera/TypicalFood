3145f098c03dfaa0a220f0d8f0ca85f5d3c54e7b
William Lopera <lopera.william@gmail.com>
2019-11-08 00:18:34 -0500

    uso de boopstrap y pase parametros entre componentes routerLink

--- src/app/food-details/food-details.component.ts
+++ src/app/food-details/food-details.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Food } from '../food';
+import { FOODS } from '../mock-foods';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-food-details',
@@ -9,11 +14,24 @@ import { Food } from '../food';
 })
 export class FoodDetailsComponent implements OnInit {
 
-  @Input() food: Food;
+  food: Food;
+  foods: Food[] = FOODS;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.food = this.foods.find(x => x.id == id);
+
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
